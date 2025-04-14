4c94e34abad243862b6b619b5aff2b7a37e7dce0
Jason Dark <jason@mail.asymptoticgroup.com>
2025-04-14 14:52:06 -0400

    tests: added case exercising basic substitution group checks

--- fts-solver/tests/samples.rs
+++ fts-solver/tests/samples.rs
@@ -1,3 +1,4 @@
+use approx::assert_abs_diff_eq;
 use fts_solver::{
     AuctionOutcome,
     cli::{BidderId, PortfolioId, ProductId, RawAuction},
@@ -8,6 +9,16 @@ use std::{fmt::Debug, fs::File, io::BufReader, path::PathBuf};
 
 mod all_solvers;
 use all_solvers::all_solvers;
+
+// This test case is actually a dynamically generated, Cartesian product of test cases.
+// For every solver implementation, and for every (input.json, output.json) pair in `./samples/**`,
+//   1. Read in the auction input,
+//   2. Read in the known-good auction output,
+//   3. Solve the auction from the input,
+//   4. Compare the solution to the known-good output.
+// Comparing the solution is done somewhat simplistically for now, but can be expanded upon later.
+// For now, the comparison requires the ordering of products, submissions, and portfolios to be preserved,
+// and checks a simple absolute-difference measure against products and portfolios in both their prices and trades.
 
 #[apply(all_solvers)]
 #[rstest]
@@ -38,8 +49,8 @@ fn cmp<BidderId: Debug + Eq, PortfolioId: Debug + Eq, ProductId: Debug + Eq>(
     assert_eq!(a.products.len(), b.products.len());
     for ((p1, o1), (p2, o2)) in a.products.iter().zip(b.products.iter()) {
         assert_eq!(p1, p2);
-        assert!((o1.trade - o2.trade).abs() <= qeps);
-        assert!((o1.price - o2.price).abs() <= peps);
+        assert_abs_diff_eq!(o1.trade, o2.trade, epsilon = qeps);
+        assert_abs_diff_eq!(o1.price, o2.price, epsilon = peps);
     }
 
     assert_eq!(a.submissions.len(), b.submissions.len());
@@ -49,8 +60,8 @@ fn cmp<BidderId: Debug + Eq, PortfolioId: Debug + Eq, ProductId: Debug + Eq>(
 
         for ((p1, o1), (p2, o2)) in s1.iter().zip(s2.iter()) {
             assert_eq!(p1, p2);
-            assert!((o1.trade - o2.trade).abs() <= qeps);
-            assert!((o1.price - o2.price).abs() <= peps);
+            assert_abs_diff_eq!(o1.trade, o2.trade, epsilon = qeps);
+            assert_abs_diff_eq!(o1.price, o2.price, epsilon = peps);
         }
     }
 }
